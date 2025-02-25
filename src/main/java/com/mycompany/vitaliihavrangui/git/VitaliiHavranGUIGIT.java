419d3bd4b4e0bd08f59e0d6756861ed19be1293a
Vitalii <vitaliihavran06@gmail.com>
2025-02-25 19:28:37 +0100

    Añadido saludo 'Bienvenido a RAMA1\!'

--- src/main/java/com/mycompany/vitaliihavrangui/git/VitaliiHavranGUIGIT.java
+++ src/main/java/com/mycompany/vitaliihavrangui/git/VitaliiHavranGUIGIT.java
@@ -8,15 +8,18 @@ public class VitaliiHavranGUIGIT extends JFrame {
         JLabel lblSaludo = new JLabel("Hola Mundo.");
         JLabel lblSaludo2 = new JLabel("¿Qué tal estás?");
         JLabel lblSaludo3 = new JLabel("¡Bienvenidos!");
+        JLabel lblSaludo4 = new JLabel("¡Bienvenido a RAMA1!"); // El nuevo saludo añadido
 
         lblSaludo.setBounds(2, 28, 116, 24); 
         lblSaludo2.setBounds(2, 50, 116, 24); 
         lblSaludo3.setBounds(2, 72, 116, 24); 
-        
+        lblSaludo4.setBounds(2, 94, 160, 24); // Posición de la nueva etiqueta
+
         this.setLayout(null); 
         this.add(lblSaludo);
         this.add(lblSaludo2);
         this.add(lblSaludo3);
+        this.add(lblSaludo4); // Añadir la nueva etiqueta a la ventana
 
         this.setSize(400, 300);
         this.setTitle("JFrame");
