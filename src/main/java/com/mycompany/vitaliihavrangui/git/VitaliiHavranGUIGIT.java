0279afdacc94f7cdf2e2ae09537f3cc44edfaf81
Vitalii <vitaliihavran06@gmail.com>
2025-02-25 19:45:25 +0100

    Primera versión de mi Proyecto!

--- src/main/java/com/mycompany/vitaliihavrangui/git/VitaliiHavranGUIGIT.java
+++ src/main/java/com/mycompany/vitaliihavrangui/git/VitaliiHavranGUIGIT.java
@@ -11,14 +11,17 @@ public class VitaliiHavranGUIGIT extends JFrame {
     public VitaliiHavranGUIGIT() { 
         JLabel lblSaludo = new JLabel("Hola Mundo.");
         JLabel lblSaludo2 = new JLabel("¿Qué tal estás?");
-        
+        JLabel lblSaludo3 = new JLabel("¡Bienvenidos a la rama RAMA1!");
+       
+       
         lblSaludo.setBounds(2, 28, 116, 24); 
         lblSaludo2.setBounds(2, 50, 116, 24); 
+        lblSaludo3.setBounds(2, 72, 180, 24);
         
         this.setLayout(null); 
         this.add(lblSaludo);
         this.add(lblSaludo2);
-
+        this.add(lblSaludo3);
         this.setSize(400, 300);
         this.setTitle("JFrame");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
