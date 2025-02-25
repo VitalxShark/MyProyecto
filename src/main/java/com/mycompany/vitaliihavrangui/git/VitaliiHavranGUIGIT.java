334593375ec6ab8594b6bf38f65032a33191c214
Vitalii <vitaliihavran06@gmail.com>
2025-02-25 17:55:22 +0100

    Hola mundo v1.1

--- src/main/java/com/mycompany/vitaliihavrangui/git/VitaliiHavranGUIGIT.java
+++ src/main/java/com/mycompany/vitaliihavrangui/git/VitaliiHavranGUIGIT.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
 package com.mycompany.vitaliihavrangui.git;
 
 import javax.swing.JFrame;
@@ -6,11 +10,15 @@ import javax.swing.JLabel;
 public class VitaliiHavranGUIGIT extends JFrame { 
     public VitaliiHavranGUIGIT() { 
         JLabel lblSaludo = new JLabel("Hola Mundo.");
-        lblSaludo.setBounds(50, 50, 100, 30); 
-
+        JLabel lblSaludo2 = new JLabel("¿Qué tal estás?");
+        
+        lblSaludo.setBounds(2, 28, 116, 24); 
+        lblSaludo2.setBounds(2, 50, 116, 24); 
+        
         this.setLayout(null); 
         this.add(lblSaludo);
-        
+        this.add(lblSaludo2);
+
         this.setSize(400, 300);
         this.setTitle("JFrame");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
